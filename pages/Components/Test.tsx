6ad1014f8980ea86dc056246ac01ac159f63bb22
Ubuntu <ubuntu@ip-172-31-26-195.eu-central-1.compute.internal>
2023-03-01 07:13:23 +0000

    ADDED: Tailwindcss to NextJS

---
+++ pages/Components/Test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <>
+      <h1 className="text-blue-500 font-bold text-7xl">Watery Water</h1>
+    </>
+  )
+}
+
+export default Test;
