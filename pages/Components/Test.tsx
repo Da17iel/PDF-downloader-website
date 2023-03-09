2f3234c7bc00fc37c7eb25ca5eb9cfa7d6de2fd3
Ubuntu <ubuntu@ip-172-31-26-195.eu-central-1.compute.internal>
2023-03-09 08:09:02 +0000

    ADDED: prisma.schema and .env-File

--- pages/Components/Test.tsx
+++ pages/Components/Test.tsx
@@ -4,6 +4,7 @@ const Test = () => {
   return (
     <>
       <h1 className="text-blue-500 font-bold text-7xl">Watery Water</h1>
+      <h2 className="mt-10 font-Posterama">This Website is owned by Watery Water Inc.</h2>
     </>
   )
 }
