03e701a9811de07a20643eebc5c3955119823b74
Devalon-Enter <faberlorin@hotmail.com>
2023-04-28 08:10:31 +0200

    ADDED: Proper API calls (GET)

--- pages/Components/ShopHub/Shop.tsx
+++ pages/Components/ShopHub/Shop.tsx
@@ -2,25 +2,37 @@ import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import prisma from "@/prismaClient";
+import { Product } from "@/types/product-types";
 
-const Shop = async () => {
-  const product = await prisma.product.findUnique({
-    where: {
-      product_id: 1,
-    },
-  })
+interface Props {
+  data: Product[];
+}
 
-  const readableProduct = JSON.stringify(product)
+const Shop = ({ data }: Props) => {
 
   return (
     <>
-      <NavBar />
-        <ul>
-          <li>{readableProduct}</li>
-        </ul>
-      <Footer />
+      <div className="h-screen bg-red-500
+      ">
+        <NavBar />
+          <ul>
+            <li key={data[0].product_id} className="h-20 w-36 text-white">{data[0].product_name}</li>
+          </ul>
+        <Footer />
+      </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/products');
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  }
 }
 
 export default Shop;
---
+++ types/product-types.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  product_id: number,
+  product_name: string,
+  product_price: number,
+  product_description: string,
+  product_liter: number,
+  product_addDate: Date,
+  product_lastModified: Date
+}
